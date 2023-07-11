1f59a89f43de11b834d4757dee733f8c3e1458bf
NithinGeorge93 <georgenithin93@gmail.com>
2023-07-11 08:36:43 +0530

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+
+export class HomePageComponent {
+  slides = [
+    { img: "asets/images/Community-1.svg" },
+    { img: "http://placehold.it/350x150/111111" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/666666" }
+  ];
+
+  slideConfig = {
+    "dots": true,
+    "infinite": true,
+    "slidesToShow": 3,
+    "slidesToScroll": 1
+  };
+
+  addSlide() {
+    this.slides.push({ img: "http://placehold.it/350x150/777777" })
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  slickInit() {
+    console.log('slick initialized');
+  }
+}
